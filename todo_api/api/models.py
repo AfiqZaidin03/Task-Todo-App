b3d00cb3b91257b10f0a93230cc4057960ab4eaa
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-03-02 16:23:47 +0800

    [3] Create model for class
    Creating model for class todo

--- todo_api/api/models.py
+++ todo_api/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length = 100)
+    desc = models.CharField(max_length = 200)
+    isDone = models.BooleanField(default = False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title[0:50]
+    
+    class Meta: 
+        ordering = ['-updated']
